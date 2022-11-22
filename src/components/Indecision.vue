d94e73d355ee1ce834104d615639aec0912ab412
Luis Falconi <luism.falconi@gmail.com>
2022-11-22 13:57:37 -0300

    modif comp. Indecision and add style css global

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="https://via.placeholder.com/250" alt="bg" />
+  <img v-if="img" :src="img" alt="bg" />
   <div class="bg-dark"></div>
 
   <div class="indecision-container">
@@ -7,7 +7,7 @@
     <p>Recuerda terminar con un signo de interrogación '?'</p>
     <div>
       <h2>{{ question }}</h2>
-      <h1>Si, No... pensando</h1>
+      <h1>{{answer}}</h1>
     </div>
   </div>
 </template>
@@ -17,11 +17,24 @@ export default {
   data() {
     return {
       question: null,
+      answer: null,
+      img: null
     };
+  },
+  methods: {
+    async getAnswer(){
+        this.answer = 'Pensando...'
+
+        const { answer, image } = await fetch('https://yesno.wtf/api').then(r => r.json())
+
+        this.answer = answer
+        this.img = image
+    }
   },
   watch: {
     question(value) {
       if (!value.includes("?")) return;
+      this.getAnswer()
     },
   },
 };
